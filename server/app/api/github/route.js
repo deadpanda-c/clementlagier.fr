a70eb9f08e67147a5aa6b871e41d5d2812e4cd2a
Clément Lagier <clement.lagier@epitech.eu>
2024-02-29 14:31:03 +0100

    [UPDATE] modified the gitignore to avoid sending the .env file

--- server/app/api/github/route.js
+++ server/app/api/github/route.js
@@ -2,6 +2,34 @@ import {
   NextResponse,
 } from 'next/server';
 
+import {
+  GITHUB_REPOS_URL,
+  GITHUB_LAST_COMMIT_URL,
+} from '../../data/constants';
+
+const getLastsCommits = async (reposNames) => {
+    var commits = {};
+    reposNames.forEach(async (repoName) => {
+      const lastCommit = await fetch(GITHUB_LAST_COMMIT_URL + repoName + '/commits?timestamp=' + Date.now())
+      .then((response) => response.json());
+
+      console.log(JSON.stringify(lastCommit, null, 2));
+
+      commits[repoName] = {
+        lastCommitDate: lastCommit[0].commit.author.date,
+        lastCommitMessage: lastCommit[0].commit.message,
+      };
+    });
+    return commits;
+}
+
 export const GET = async () => {
-  return NextResponse.json({ message: 'Hello, World!' });
+  const repos = await fetch(GITHUB_REPOS_URL + '&timestamp=' + Date.now())
+  .then((response) => response.json())
+  const reposNames = repos.items.map((repo) => repo.name);
+  const lastCommit = await getLastsCommits(reposNames);
+
+  console.log(JSON.stringify(lastCommit, null, 2));
+
+  return NextResponse.json({hello: 'world'});
 };
--- server/app/data/constants.js
+++ server/app/data/constants.js
@@ -1,3 +1,4 @@
-export const GITHUB_API_URL = process.env.REACT_APP_GITHUB_API_URL;
+export const GITHUB_REPOS_URL = process.env.GITHUB_REPOS_URL;
+export const GITHUB_LAST_COMMIT_URL = process.env.GITHUB_LAST_COMMIT_URL;
 export const API_URL = process.env.API_URL;
 export const ABOUT_URL = process.env.ABOUT_URL;
