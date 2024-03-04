505a89d347c7596c42a0967f282498a56af36244
Clément Lagier <clement.lagier@epitech.eu>
2024-03-04 11:44:41 +0100

    [ADD] Added the prisma database for the access token

--- server/app/api/github/login/callback/route.js
+++ server/app/api/github/login/callback/route.js
@@ -1,7 +1,45 @@
 import {
+  PrismaClient,
+} from '@prisma/client';
+
+import {
   NextResponse,
 } from 'next/server';
 
+import {
+  GITHUB_TOKEN_URL,
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+} from '../../../../data/constants';
+
 export const GET = async (req) => {
+  const url = new URL(req.url);
+  const code = url.searchParams.get('code');
+
+  const token = await fetch(GITHUB_TOKEN_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'User-Agent': 'next.js',
+    },
+    body: JSON.stringify({
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  }).then((res) => res.json());
+  
+  const prisma = new PrismaClient();
+
+  const user = await prisma.user.create({
+    data: {
+      access_token: token.access_token,
+      token_type: token.token_type,
+      scopes: token.scope,
+    },
+  });
+
+  console.log(user);
   return NextResponse.json({ message: 'Hello, World!' });
 };
--- server/app/data/constants.js
+++ server/app/data/constants.js
@@ -9,3 +9,4 @@ export const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI;
 
 export const GITHUB_API_URL = process.env.GITHUB_API_URL;
 export const GITHUB_AUTH_URL = process.env.GITHUB_AUTH_URL;
+export const GITHUB_TOKEN_URL = process.env.GITHUB_TOKEN_URL;
