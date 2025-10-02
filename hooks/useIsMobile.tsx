61aec353be012fcf3b1551c968fcb8f89d7b7259
Clément Lagier <clement.lagier@epitech.eu>
2025-10-02 02:48:10 +0200

    [ADD] Created a hook to check if the user is using a mobile device or a desktop

---
+++ hooks/useIsMobile.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreen = () => setIsMobile(window.innerWidth < 768);
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
+
+  return isMobile;
+}
+
+export default useIsMobile;
