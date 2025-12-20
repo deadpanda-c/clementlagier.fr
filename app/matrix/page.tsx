31495c028988e700251e4b1a613052feb0dd7b25
Clément Lagier <clement.lagier@epitech.eu>
2025-12-20 23:46:56 +0100

    [BACKUP]

--- app/matrix/page.tsx
+++ app/matrix/page.tsx
@@ -27,7 +27,7 @@ const HomePage = () => {
   const [commandHistory, setCommandHistory] = useState<Array<CommandSet>>([]);
   const [currentCommandSetIndex, setCurrentCommandSetIndex] = useState(0);
   const repos = useGithubRepos();
-  const cmd_style = "text-white flex ml-2 text-bold text-sm md:text-xl";
+  const cmd_style = "text-white flex ml-2 text-bold text-sm md:text-xl w-full";
 
   useEffect(() => {
     const handleResize = () => {
@@ -41,57 +41,6 @@ const HomePage = () => {
 
 
 
-const infos_system = useMemo(() => {
-  const infos = [
-    {
-      text: "[User]",
-      delay: 200,
-      className: cmd_style,
-      isPreformatted: false,
-      velocity: 0.2
-    },
-    {
-      text: "name = Clement Lagier",
-      delay: 200,
-      className: "text-white text-xl",
-      isPreformatted: false,
-      velocity: 0.2
-    },
-    {
-      text: "location = Moulins, France",
-      delay: 200,
-      className: "text-white text-xl",
-      isPreformatted: false,
-      velocity: 0.2
-    },
-    {
-      text: "email = clement.lagier@epitech.eu",
-      delay: 200,
-      className: "text-white text-xl hover:text-green-400 hover:underline cursor-pointer",
-      isPreformatted: false,
-      velocity: 0.2,
-      link: "mailto:clement.lagier@epitech.eu"
-    },
-    {
-      text: "github = https://github.com/deadpanda-c",
-      delay: 200,
-      className: "text-white text-xl hover:text-green-400 hover:underline cursor-pointer",
-      isPreformatted: false,
-      velocity: 0.2,
-      link: "https://github.com/deadpanda-c"
-    },
-    {
-      text: "linkedin = https://www.linkedin.com/in/clement-lagier",
-      delay: 200,
-      className: "text-white text-xl hover:text-green-400 hover:underline cursor-pointer",
-      isPreformatted: false,
-      velocity: 0.2,
-      link: "https://www.linkedin.com/in/clement-lagier"
-    },
-  ];
-  return infos;
-}, [cmd_style]);
-
 const allCommandSets = useMemo(() => {
   const baseSets = [
     [
@@ -103,23 +52,9 @@ const allCommandSets = useMemo(() => {
         velocity: 0.2
       },
       {
-        text: "-----------------------------",
-        delay: 200,
-        className: "text-green-400 text-xs md:text-base",
-        isPreformatted: false,
-        velocity: 0.2
-      },
-      {
-        text: "I'm a software engineer who is passionate about building new things.",
-        delay: 400,
-        className: "text-white text-sm md:text-base",
-        isPreformatted: false,
-        velocity: 0.2
-      },
-      {
-        text: "-----------------------------",
-        delay: 200,
-        className: "text-green-400 text-xs md:text-base",
+        text: "Hello World ! I'm passionate about pentesting and learning new things. Enjoy your visit !",
+        delay: 100,
+        className: "text-white text-[0.75rem] md:text-xl",
         isPreformatted: false,
         velocity: 0.2
       },
@@ -127,29 +62,29 @@ const allCommandSets = useMemo(() => {
     [
       {
         text: "ls -l ./skills/",
-        delay: 0,
+        delay: 200,
         className: cmd_style,
         isPreformatted: false,
         velocity: 0.2
       },
       {
-        text: ".rw-r--r-- deadpanda deadpanda c",
-        delay: 200,
-        className: "text-cyan-400 text-[0.75rem] md:text-xl",
+        text: ".rw-r--r-- deadpanda deadpanda Low-level programming languages (C, C++, Rust)",
+        delay: 300,
+        className: "text-white text-[0.75rem] md:text-xl",
         isPreformatted: false,
         velocity: 0.2,
       },
       {
-        text: ".rw-r--r-- deadpanda deadpanda c++",
-        delay: 200,
-        className: "text-cyan-400 text-[0.75rem] md:text-xl",
+        text: ".rw-r--r-- deadpanda deadpanda Scripting languages (Python, Bash, JavaScript)",
+        delay: 300,
+        className: "text-white text-[0.75rem] md:text-xl",
         isPreformatted: false,
         velocity: 0.2,
       },
       {
-        text: ".rw-r--r-- deadpanda deadpanda nextjs",
-        delay: 200,
-        className: "text-cyan-400 text-[0.75rem] md:text-xl",
+        text: ".rw-r--r-- deadpanda deadpanda SoftSkills (Communication, Teamwork)",
+        delay: 300,
+        className: "text-white text-[0.75rem] md:text-xl",
         isPreformatted: false,
         velocity: 0.2
       }
@@ -158,34 +93,27 @@ const allCommandSets = useMemo(() => {
 
     const projectSet = [
       {
-        text: "./projects --list --verbose",
+        text: "repo --pinned",
         delay: 0,
         className: cmd_style,
         isPreformatted: false,
         velocity: 0.2
       },
+      {
+        text: "NAME        LANGUAGE         DESCRIPTION",
+        delay: 0,
+        className: cmd_style + " whitespace-pre-wrap",
+        isPreformatted: false,
+        velocity: 0.2
+      },
       ...repos.flatMap((repo, index) => [
         {
-          text: "----------------",
+          text: `${repo.name}        ${repo.language}        ${repo.description || "No description"}`,
           delay: 200,
-          className: "text-green-400 text-xs md:text-base",
+          className: "text-white text-sm md:text-lg whitespace-pre-wrap",
           isPreformatted: false,
           velocity: 0.2,
-        },
-        {
-          text: `PROJECT_NAME=${repo.name}`,
-          delay: 200,
-          className: "text-white text-sm md:text-lg",
-          isPreformatted: false,
-          velocity: 0.2,
-          link: `${repo.link}`
-        },
-        {
-          text: `DESCRIPTION=${repo.description || "No description"}`,
-          delay: 200,
-          className: "text-white text-[0.8rem] md:text-base break-all  whitespace-pre-wrap",
-          isPreformatted: false,
-          velocity: 0.2,
+          link: `https://github.com/${repo.author}/${repo.name}`
         },
       ])
     ];
@@ -196,10 +124,12 @@ const allCommandSets = useMemo(() => {
   useEffect(() => {
     
     // Start the first command set
-    setCommandHistory([{
-      id: 0,
-      lines: allCommandSets[0]
-    }]);
+    const timer = setTimeout(() => {
+      setCommandHistory([{
+        id: 0,
+        lines: allCommandSets[0]
+      }]);
+    }, 300);
   }, [allCommandSets]);
 
   const handleCommandSetComplete = () => {
@@ -214,6 +144,7 @@ const allCommandSets = useMemo(() => {
         id: prev.length,
         lines: allCommandSets[nextIndex]
       }]);
+      console.log(allCommandSets[nextIndex]);
     }
   };
 
@@ -238,61 +169,27 @@ const allCommandSets = useMemo(() => {
       `}</style>
       <div className="flex flex-row min-h-screen bg-black divide-x-2">
 
-      {/* Left side - Terminal */}
-      <div className="w-full md:w-3/5 overflow-x-auto bg-black text-green-400 font-mono p-4 md:p-8 flex flex-col md:overflow-y-auto md:h-screen scrollbar-hide">
-        <TerminalHeader displayPrompt={false} pwd="/matrix"/>
-        <div className="flex-1 min-w-0 break-words ">
-          {commandHistory.map((commandSet, index) => (
-            <div key={commandSet.id} className="mb-6">
-              <div className="flex gap-2 items-start">
-                <Prompt pwd="/matrix" isMobile={isMobile} />
-                <div>
-                  <TypingText 
-                    lines={commandSet.lines}
-                    onComplete={index === commandHistory.length - 1 ? handleCommandSetComplete : undefined}
-                  />
+        {/* Left side - Terminal */}
+        <div className="w-full md:w-3/5 overflow-x-auto bg-black text-green-400 font-mono p-4 md:p-8 flex flex-col md:overflow-y-auto md:h-screen scrollbar-hide">
+          <TerminalHeader displayPrompt={false} pwd="/matrix"/>
+          <div className="flex-1 min-w-0 break-words">
+            {commandHistory.map((commandSet, index) => (
+              <div key={commandSet.id} className="mb-6">
+                <div className="flex gap-2 items-start">
+                  <Prompt pwd="/matrix" isMobile={isMobile} />
+                  <div>
+                    <TypingText 
+                      lines={commandSet.lines}
+                      onComplete={index === commandHistory.length - 1 ? handleCommandSetComplete : undefined}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Right side - Info System */}
-      { !isMobile && (
-      <div 
-        id="info-section"
-        className="w-2/5 fixed right-0 top-16 h-screen bg-black p-4 p-8 flex flex-col font-mono"
-      >
-        <div className="w-full">
-          <div className="mb-4 flex">
-            <Prompt pwd="/matrix" isMobile={isMobile} />
-            <TypingText
-              lines={[
-                {
-                  text: "cat .deadpanda.conf",
-                  delay: 200,
-                  className: cmd_style,
-                  isPreformatted: false,
-                  velocity: 0.2
-                }
-              ]} 
-            />
+            ))}
           </div>
-          {infos_system.map((info, index) => (
-            <div key={index} className="mb-4">
-              <div className="flex gap-2 items-start">
-                <div>
-                  <TypingText 
-                    lines={[info]}
-                  />
-                </div>
-              </div>
-            </div>
-          ))}
         </div>
-      </div>
-        )}
+
+        {/* Right side - Info System */}
     </div>
     </>
   );
--- hooks/useGithubRepos.tsx
+++ hooks/useGithubRepos.tsx
@@ -5,10 +5,16 @@ import { useEffect, useState } from "react";
 const useGithubRepos = () => {
   const [repos, setRepos] = useState<{ name: string; link: string; description: string }[]>([]);
 
+
   useEffect(() => {
     const fetchRepos = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/repos`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_PINNED_REPOS_URL}`, {
+          cache: 'no-store',
+          headers: {
+            'Cache-Control': 'no-cache',
+          }
+        });
         const data = await response.json();
         setRepos(data);
       } catch (error) {
